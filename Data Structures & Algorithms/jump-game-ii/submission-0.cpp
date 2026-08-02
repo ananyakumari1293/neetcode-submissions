ee754569d9f64a60e968e6728e4430672116cadc
Ananya Kumari <ananyakumari1293@gmail.com>
2026-08-02 16:38:46 +0530

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps=0,l=0,r=0;
+        while(r<nums.size()-1){
+            int far=0;
+            for(int i=l;i<=r;i++){
+                far=max(nums[i]+i,far);
+                
+            }
+            l=r+1;
+            r=far;
+            jumps+=1;
+        }
+        return jumps;
+        
+    }
+};
